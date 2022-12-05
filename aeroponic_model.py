a306bb1a27970cc8a49d3ecca235a50e009c093f
Joaquin Peraza <joaquin@peraza.uy>
2022-12-05 10:12:36 -0600

    different approach

--- aeroponic_model.py
+++ aeroponic_model.py
@@ -3,99 +3,126 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.interpolate import CubicSpline
 import warnings
+from scipy.optimize import curve_fit
 # Read df
 df_water_times = pd.read_excel('Data.xlsx', sheet_name='water_times', skiprows=[1])
 df_water_flow = pd.read_excel('Data.xlsx', sheet_name='water_flow', skiprows=[1])
 df_radiation = pd.read_excel('Data.xlsx', sheet_name='radiation', skiprows=[1])
+df_lai = pd.read_excel('Data.xlsx', sheet_name='leaf_area_biomass', skiprows=[1])
+df_dm = pd.read_excel('Data.xlsx', sheet_name='fresh_biomass_DM', skiprows=[1])
+df_biomass_curves = pd.read_excel('Data.xlsx', sheet_name='biomass', skiprows=[1])
 
 print(df_water_times)
 print(df_water_flow)
+print(df_radiation)
+print(df_lai)
+print(df_dm)
 class AeroponicModel:
     def __init__(self):
-        self.water_flow_losses = None
         self.water_times_losses = None
+        self.water_flow_losses = None
         self.radiation_losses = None
+        self.biomass_to_leaf = None
+        self.fresh_biomass_to_dry_biomass = None
+        self.growing_curves = None
 
     def calibrate(self, plot = False):
-        global Water_flow_losses, Water_times_losses
-        # Calibrate biomass loss due to water flow
-        df_water_flow['loss'] = df_water_flow['biomass_fresh']/df_water_flow['biomass_fresh'].max() - 1
-        # Fit spline to loss and rate
-        loss = df_water_flow['loss']
-        rate = df_water_flow['rate']
-        # Fit spline
-        cs_flow = CubicSpline(rate, loss)
+
+        # Calibrate biomass to leaf area
+        self.biomass_to_leaf = np.polynomial.Chebyshev.fit(df_lai['fresh_biomass'], df_lai['leaf_area'], 5)
+        if plot:
+            plt.title('Biomass to leaf area')
+            plt.plot(df_lai['fresh_biomass'], df_lai['leaf_area'], 'o', label='data')
+            plt.plot(np.linspace(0, 200, 100), self.biomass_to_leaf(np.linspace(0, 200, 100)), label='chebyshev')
+            plt.legend(loc='best')
+            plt.xlabel('Fresh biomass (g/plant)')
+            plt.ylabel('Leaf area (cm2/plant)')
+            plt.show()
+
+        # Calibrate fresh biomass to dry biomass
+        self.fresh_biomass_to_dry_biomass = np.polynomial.Chebyshev.fit(df_dm['fresh_biomass'], df_dm['dry_biomass'], 4)
+        if plot:
+            plt.title('Fresh biomass to dry biomass')
+            plt.plot(df_dm['fresh_biomass'], df_dm['dry_biomass'], 'o', label='data')
+            plt.plot(np.linspace(0, 200, 100), self.fresh_biomass_to_dry_biomass(np.linspace(0, 200, 100)), label='chebyshev')
+            plt.legend(loc='best')
+            plt.xlabel('Fresh biomass (g/plant)')
+            plt.ylabel('Dry biomass (g/plant)')
+            plt.show()
+
+        # Calibrate growing curves
+        #sort by radiation and group by radiation
+        df_biomass_grouped = df_biomass_curves.sort_values(by=['radiation']).groupby('radiation')
+        # Fit splines to each group
+        self.growing_curves = {}
+        sigmoid = lambda x, a, b, c, d: a / (1 + np.exp(-b * (x - c))) + d
+        for radiation, group in df_biomass_grouped:
+            # Fit spline
+            df = group.sort_values(by=['day'])
+            params = curve_fit(sigmoid, df['day'], df['biomass_dry'], p0=[0, 1, 20, 40], maxfev=10000)
+            # Store spline
+            self.growing_curves[radiation] = params[0]
         # Plot
         if plot:
-            plt.title('Biomass loss due to water flow')
-            plt.plot(rate, loss, 'o', label='data')
-            plt.plot(np.linspace(0, 2, 100), cs_flow(np.linspace(0, 2, 100)), label='spline')
-            plt.xlabel('Water flow rate (L/h)')
-            plt.ylabel('Biomass loss (fraction)')
+            plt.title('Growing curves')
+            for i in self.growing_curves:
+                plt.plot(np.linspace(11, 50, 100), sigmoid(np.linspace(11, 50, 100), *self.growing_curves[i]), label='Scenario = ' + str(i))
             plt.legend(loc='best')
+            plt.xlabel('Time (days)')
+            plt.ylabel('Biomass (g/plant)')
             plt.show()
-        self.water_flow_losses = cs_flow
+            # Plot derivatives
+            plt.title('Growing curves derivatives')
+            for i in self.growing_curves:
+                derivative = lambda x: sigmoid(x, *self.growing_curves[i]) * self.growing_curves[i][1] * self.growing_curves[i][0] * np.exp(-self.growing_curves[i][1] * (x - self.growing_curves[i][2])) / (1 + np.exp(-self.growing_curves[i][1] * (x - self.growing_curves[i][2]))) ** 2
+                plt.plot(np.linspace(11, 50, 100), derivative(np.linspace(11, 50, 100)), label=f"Scenario {i}")
+            plt.legend(loc='best')
+            plt.xlabel('Time (days)')
+            plt.ylabel('Biomass derivative (g/plant/day)')
+            plt.show()
 
-        # Calibrate biomass loss due to water times
-        df_water_times['loss'] = df_water_times['biomass_dry']/df_water_times['biomass_dry'].max() - 1
-        # Fit spline to loss and rate
-        loss = df_water_times['loss']
-        off_time = df_water_times['off']
-        cs_times = CubicSpline(off_time, loss)
+        # Get growing curves for each water times
+        bet_scenario_params = self.growing_curves[22]
+        time_curves = {}
+        days = df_water_times['days'].mean()
+        # Group water times by scenario
+        for group, df in df_water_times.groupby('off'):
+            # Get growing curve
+            params = bet_scenario_params
+            params[0] = df['biomass_dry']
+            print(f"Scenario {group} params: {params}")
+            # Store growing params
+            time_curves[group] = np.array(params)
         # Plot
         if plot:
-            plt.title('Water timing losses')
-            plt.plot(off_time, loss, 'o', label='data')
-            plt.plot(np.linspace(0, 60, 100), cs_times(np.linspace(0, 60, 100)), label='spline')
+            plt.title('Growing curves for each water times')
+            for group in time_curves:
+                plt.plot(np.linspace(11, days, 100), sigmoid(np.linspace(11, days, 100), *time_curves[group]), label='Water times = ' + str(group))
             plt.legend(loc='best')
-            plt.xlabel('Watering interval (min)')
-            plt.ylabel('Biomass loss (fraction)')
+            plt.xlabel('Time (days)')
+            plt.ylabel('Biomass (g/plant)')
             plt.show()
-        self.water_times_losses = cs_times
 
-        # Calibrate biomass loss due to radiation
-        df_radiation['loss'] = df_radiation['biomass_fresh']/df_radiation['biomass_fresh'].max() - 1
-        # Fit spline to loss and rate
-        loss = df_radiation['loss']
-        radiation = df_radiation['radiation']
-        cs_radiation = CubicSpline(radiation, loss)
+        # Get growing curves for each water flow
+        bet_scenario_params = self.growing_curves[22]
+        flow_curves = {}
+        # Group water flow by scenario
+        days = df_water_flow['days'].mean()
+        for group, df in df_water_flow.groupby('rate'):
+            # Get growing curve
+            params = bet_scenario_params
+            params[0] = self.fresh_biomass_to_dry_biomass(df['biomass_fresh'])
+            print(f"Scenario {group} params: {params}")
+            # Store growing params
+            flow_curves[group] = np.array(params)
         # Plot
         if plot:
-            plt.title('Radiation losses')
-            plt.plot(radiation, loss, 'o', label='data')
-            plt.plot(np.linspace(0, 400, 100), cs_radiation(np.linspace(0, 400, 100)), label='spline')
+            plt.title('Growing curves for each water flow')
+            for group in flow_curves:
+                plt.plot(np.linspace(11, days, 100), sigmoid(np.linspace(11, days, 100), *flow_curves[group]), label='Water flow = ' + str(group))
             plt.legend(loc='best')
-            plt.xlabel('Light flux (umol/m2/s)')
-            plt.ylabel('Biomass loss (fraction)')
+            plt.xlabel('Time (days)')
+            plt.ylabel('Biomass (g/plant)')
             plt.show()
-        self.radiation_losses = cs_radiation
-
-    def get_loss(self, water_flow, water_times, radiation):
-        if water_flow > df_water_flow['rate'].max():
-            warnings.warn(f'Water flow rate is outside the scope of the calibration data. Max value is {df_water_flow["rate"].max()}')
-        if water_times > df_water_times['off'].max():
-            warnings.warn(f'Watering interval is outside the scope of the calibration data. Max value is {df_water_times["off"].max()}')
-        if radiation > df_radiation['radiation'].max():
-            warnings.warn(f'Radiation is outside the scope of the calibration data. Max value is {df_radiation["radiation"].max()}')
-        if water_flow < df_water_flow['rate'].min():
-            warnings.warn(f'Water flow rate is outside the scope of the calibration data. Min value is {df_water_flow["rate"].min()}')
-        if water_times < df_water_times['off'].min():
-            warnings.warn(f'Watering interval is outside the scope of the calibration data. Min value is {df_water_times["off"].min()}')
-        if radiation < df_radiation['radiation'].min():
-            warnings.warn(f'Radiation is outside the scope of the calibration data. Min value is {df_radiation["radiation"].min()}')
-
-
-        # Calculate biomass loss due to water flow
-        loss_flow = self.water_flow_losses(water_flow)
-        # Calculate biomass loss due to
-        loss_times = self.water_times_losses(water_times)
-        # Calculate biomass loss due to
-        loss_radiation = self.radiation_losses(radiation)
-        # Calculate total biomass loss
-        loss_total = loss_flow + loss_times + loss_radiation
-        return loss_total
-
-
-
 
 
